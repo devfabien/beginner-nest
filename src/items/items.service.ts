702bf4b634b7e8dc9be12ee623040eb79219f773
devfabien <ish001fabi@gmail.com>
2023-11-30 10:52:51 +0200

    feat: creating objects and functions

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '123456',
+      name: 'Item one',
+      description: 'This is item one',
+      quantity: 100,
+    },
+    {
+      id: '268336',
+      name: 'Item two',
+      description: 'This is item two',
+      quantity: 100,
+    },
+  ];
+  findAll(): Item[] {
+    return this.items;
+  }
+  findOne(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+}
