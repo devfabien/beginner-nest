eb3a87a480a0c2bcb68d5862584faa9fdcbda023
devfabien <ish001fabi@gmail.com>
2023-11-30 08:57:34 +0200

    refactor: rendering the service items

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsController } from './items/items.controller';
+import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [MongooseModule.forRoot()],
