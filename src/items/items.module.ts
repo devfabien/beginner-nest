22b72d1742c611964cf68ebea512e411c469c179
devfabien <ish001fabi@gmail.com>
2023-11-30 12:31:02 +0200

    feat: importing the schema

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ItemSchema } from './schemas/item.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
