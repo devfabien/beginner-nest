60cd3a09dd831256f14824c3122973542261cca8
devfabien <ish001fabi@gmail.com>
2023-11-30 11:02:44 +0200

    refactor: integrating with the mongo database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/keys';
 
 @Module({
   imports: [MongooseModule.forRoot()],
