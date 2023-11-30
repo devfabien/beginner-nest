e8c8da1c3f3a25fbe4da3c308aa814550acace9a
devfabien <ish001fabi@gmail.com>
2023-11-30 10:53:34 +0200

    refactor: updating the function to findAll items and findOne item

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,16 +8,20 @@ import {
   Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  findAll(): string {
-    return 'Get all items';
+  findAll(): Item[] {
+    return this.itemsService.findAll();
   }
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item ${id}`;
+  findOne(@Param('id') id): Item {
+    return this.itemsService.findOne(id);
   }
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
