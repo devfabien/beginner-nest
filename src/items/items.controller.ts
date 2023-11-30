b0596ee327c5740c689003eeb84787f8176db89a
devfabien <ish001fabi@gmail.com>
2023-11-30 08:19:44 +0200

    feat : creating a post request

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all items';
+  }
+  @Post()
+  create(): string {
+    return 'Create item';
   }
 }
