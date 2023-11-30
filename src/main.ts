aea77851fb95cf5f8ef52304c9a18ea6191f6ae9
devfabien <ish001fabi@gmail.com>
2023-11-30 08:00:37 +0200

    refactor: removing console.log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
-  console.log('localhost:3001');
 }
 bootstrap();
