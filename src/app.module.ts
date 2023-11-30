7bf8c4210143e11c5318495b7232808c723a6e97
devfabien <ish001fabi@gmail.com>
2023-11-30 10:14:25 +0200

    refactor: going back to the default setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://ishimwefabien1:Qwerty123@cluster0.klbowej.mongodb.net/',
-    ),
-  ],
+  imports: [],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
