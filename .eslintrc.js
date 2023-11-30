371063ecc120c0270df88c60e15b8028ff0a0e5c
devfabien <ish001fabi@gmail.com>
2023-11-30 10:35:22 +0200

    refactor: modifying the eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,13 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+    
   rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  
 };
