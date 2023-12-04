8f72eaa3ab5ce4b6c89a917824361744b76f68d6
devfabien <ish001fabi@gmail.com>
2023-12-04 12:23:14 +0200

    feat: adding a test for findAll function

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsService } from './items.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Item } from './interfaces/item.interface';
 
 describe('ItemsService', () => {
   let service: ItemsService;
+  let model: Model<Item>;
+  const mockItem = {
+    _id: '656859e9cd1b3375945adcaa',
+    name: 'Item two',
+    quantity: 28,
+    __v: 0,
+    description: 'this is item two',
+  };
+  const mockItemService = { find: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        {
+          provide: getModelToken('Item'),
+          useValue: mockItemService,
+        },
+      ],
     }).compile();
-
     service = module.get<ItemsService>(ItemsService);
+    model = module.get<Model<Item>>(getModelToken('Item'));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('find', () => {
+    it('Should return all items', async () => {
+      jest.spyOn(model, 'find').mockResolvedValue([mockItem]);
+      const output = await service.findAll();
+      expect(output).toEqual([mockItem]);
+    });
   });
 });
