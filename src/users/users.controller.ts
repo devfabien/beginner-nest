5a3724b9453241913f8cf7e6a201b3e553eeae23
devfabien <ish001fabi@gmail.com>
2023-12-05 13:13:16 +0200

    feat: creating get all endpoint for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Users } from './schemas/users.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userServices: UsersService) {}
+
+  @Get()
+  getAllUsers(): Promise<Users[]> {
+    return this.userServices.findAll();
+  }
 }
