454cefa034150338319f7fc716c80fbd9c19819a
devfabien <ish001fabi@gmail.com>
2023-12-05 16:35:10 +0200

    feat: creating a sign in function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Users } from 'src/users/schemas/users.schema';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersServices: UsersService) {}
+
+  async signIn(userName: string, pass: string): Promise<Users> {
+    const user = await this.usersServices.findOne(userName);
+    if (user.password !== pass) {
+      throw new UnauthorizedException('No user with those credentials');
+    }
+
+    return user;
+  }
+}
