40fc01a98c664a6ba741fab3e5ae775da64b9b40
devfabien <ish001fabi@gmail.com>
2023-12-05 18:45:24 +0200

    feat: creating an endpoint for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  signIn(@Body() signInDto) {
+    return this.authService.signIn(signInDto.userName, signInDto.password);
+  }
+}
