b37b7b656dbb9cbe068fc0d45c481da1608c5f54
devfabien <ish001fabi@gmail.com>
2023-12-05 15:51:05 +0200

    feat: importing the auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import config from '../config/keys';
 
 @Module({
-  imports: [ItemsModule, UsersModule, MongooseModule.forRoot(config.mongoURI)],
+  imports: [
+    ItemsModule,
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(config.mongoURI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
