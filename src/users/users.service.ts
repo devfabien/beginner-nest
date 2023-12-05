82fb8a66745c02aadc8740eb131172116dd068c6
devfabien <ish001fabi@gmail.com>
2023-12-05 13:04:55 +0200

    feat: creating the constructor function for the services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Users } from './schemas/users.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(Users.name) private readonly userModel: Model<Users>,
+  ) {}
+}
