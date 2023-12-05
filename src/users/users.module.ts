a001f8a75bc9646a9fa0e45790f0cb1aee9a8730
devfabien <ish001fabi@gmail.com>
2023-12-05 13:03:32 +0200

    feat: importing the schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/users.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Users', schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
