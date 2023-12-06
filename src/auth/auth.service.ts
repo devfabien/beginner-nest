8ca11e43bc35f7baa90f2f509fdfc9d701ffe040
devfabien <ish001fabi@gmail.com>
2023-12-06 07:52:29 +0200

    refactor: modifying the signin function to return a jwt key

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { Users } from 'src/users/schemas/users.schema';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersServices: UsersService) {}
+  constructor(
+    private readonly usersServices: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(userName: string, pass: string): Promise<Users> {
+  async signIn(userName: string, pass: string) {
     const user = await this.usersServices.findOne(userName);
     if (user?.password !== pass) {
       throw new UnauthorizedException('No user with those credentials');
     }
 
-    return user;
+    const payload = { userName: user?.userName };
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
