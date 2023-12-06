4a317f4f1b95869071f1c41b3f61dc21e405571f
devfabien <ish001fabi@gmail.com>
2023-12-06 08:49:46 +0200

    refactor: adding the public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post()
   signIn(@Body() signInDto) {
     return this.authService.signIn(signInDto.userName, signInDto.password);
