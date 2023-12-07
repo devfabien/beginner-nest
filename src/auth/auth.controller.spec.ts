af8b6aaaa91e5eaabc15db44eea8c26532496990
devfabien <ish001fabi@gmail.com>
2023-12-07 17:20:57 +0200

    refactor: removing unused variables and imports

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,21 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { CreateUseDto } from 'src/users/dto/create-user.dto';
 
 describe('AuthController', () => {
   let authController: AuthController;
   let authService: AuthService;
   const jwtToken = 'jwtToken';
 
-  const mockUser = {
-    _id: '656f27642a6e033f96f73f17',
-    userName: 'Fabien',
-    password: 'fabien123',
-    createdAt: '2023-12-05T13:36:36.575Z',
-    updatedAt: '2023-12-05T13:36:36.575Z',
-    __v: 0,
-  };
   const mockAuthService = {
     signIn: jest.fn().mockResolvedValueOnce(jwtToken),
   };
