6da54b8f9ebed7b0b95350b8711d2e3a18257809
devfabien <ish001fabi@gmail.com>
2023-12-07 08:42:50 +0200

    feat: creating the test to check if the authService is defined

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Model } from 'mongoose';
+import { Users } from '../users/schemas/users.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let model: Model<Users>;
+  let jwtService: JwtService;
+
+  const mockUser = {
+    _id: '656f27642a6e033f96f73f17',
+    userName: 'Fabien',
+    password: 'fabien123',
+    createdAt: '2023-12-05T13:36:36.575Z',
+    updatedAt: '2023-12-05T13:36:36.575Z',
+    __v: 0,
+  };
+
+  const mockAuthService = {
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        UsersService,
+        JwtService,
+        AuthService,
+        { provide: getModelToken(Users.name), useValue: mockAuthService },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    model = module.get<Model<Users>>(getModelToken(Users.name));
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
