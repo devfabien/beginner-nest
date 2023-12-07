dfb542adbad61762ce58c285e0284382fb3c2103
devfabien <ish001fabi@gmail.com>
2023-12-07 08:42:04 +0200

    refactor: changing the import method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly usersServices: UsersService,
+    private usersServices: UsersService,
     private jwtService: JwtService,
   ) {}
 
