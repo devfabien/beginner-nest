639718d2bbd9fa29ae574f44380f767ec3059f11
devfabien <ish001fabi@gmail.com>
2023-12-07 15:53:54 +0200

    refactor: including the async keyword on each function since they are returning a promise

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -20,24 +20,24 @@ export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll(): Promise<Item[]> {
+  async findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<Item> {
+  async findOne(@Param('id') id: string): Promise<Item> {
     return this.itemsService.findOne(id);
   }
   @Post()
   @UsePipes(new ValidationPipe())
-  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+  async create(@Body() createItemDto: CreateItemDto): Promise<Item> {
     return this.itemsService.create(createItemDto);
   }
   @Delete(':id')
-  delete(@Param('id') id: string): Promise<Item> {
+  async delete(@Param('id') id: string): Promise<Item> {
     return this.itemsService.delete(id);
   }
   @Put(':id')
-  update(
+  async update(
     @Body() updateItemDto: CreateItemDto,
     @Param('id') id: string,
   ): Promise<Item> {
